1dcd2f64edb698787aecc4994f0295493919de8a
Astin Tiddy <Astin8@hotmail.co.nz>
2016-02-23 11:49:17 +1300

    Completed practical 1.2a

--- Weekly Practicals/Week 1/FirstAndroidApp/app/src/main/java/bit/tiddaj1/firstandroidapp/MainActivity.java
+++ Weekly Practicals/Week 1/FirstAndroidApp/app/src/main/java/bit/tiddaj1/firstandroidapp/MainActivity.java
@@ -2,6 +2,9 @@ package bit.tiddaj1.firstandroidapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView
+        TextView txtRandomString = (TextView)findViewById(R.id.dogBreed);
+
+        String dogBreed = "";
+
+        Random rand = new Random();
+        int dogNum = rand.nextInt(4);
+
+        switch(dogNum)
+        {
+            case 0:
+                dogBreed = "Pug";
+                break;
+
+            case 1:
+                dogBreed = "German Shepard";
+                break;
+
+            case 2:
+                dogBreed = "Malamute";
+                break;
+
+            case 3:
+                dogBreed = "Boxer";
+                break;
+        }
+
+        txtRandomString.setText(dogBreed);
     }
 }
