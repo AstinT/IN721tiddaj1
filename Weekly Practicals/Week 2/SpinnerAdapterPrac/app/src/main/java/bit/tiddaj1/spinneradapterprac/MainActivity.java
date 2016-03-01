9caa4fdf74b1862bc85c4227bb54b393bd834dc1
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-01 20:40:07 +1300

    Finished practical

--- Weekly Practicals/Week 2/SpinnerAdapterPrac/app/src/main/java/bit/tiddaj1/spinneradapterprac/MainActivity.java
+++ Weekly Practicals/Week 2/SpinnerAdapterPrac/app/src/main/java/bit/tiddaj1/spinneradapterprac/MainActivity.java
@@ -2,8 +2,13 @@ package bit.tiddaj1.spinneradapterprac;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,8 +39,37 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> monthsAdapter = new ArrayAdapter<String>(this, layoutID, months);
 
         //Declaring spinner
-        Spinner monthtsSpinner = (Spinner) findViewById(R.id.spnMonths);
+        Spinner monthsSpinner = (Spinner) findViewById(R.id.spnMonths);
         //setting adapter to spinner
-        monthtsSpinner.setAdapter(monthsAdapter);
+        monthsSpinner.setAdapter(monthsAdapter);
+
+        Button btnEnroll = (Button) findViewById(R.id.btnEnroll);
+        btnEnroll.setOnClickListener(new buttonListener());
+    }
+
+    //Button listener class
+    public class buttonListener implements Button.OnClickListener {
+
+        @Override
+        //onClick method.
+        //Executes when button is clicked
+        public void onClick(View v) {
+            //getting radio group id
+            RadioGroup rgInstruments = (RadioGroup) findViewById(R.id.rgInstruments);
+            //gets checked radiobutton that belongs to rgInstruments
+            int checkedId = rgInstruments.getCheckedRadioButtonId();
+            //Declaring a radioButton with the checkedId
+            RadioButton checkedRadiobtn = (RadioButton) findViewById(checkedId);
+
+            Spinner monthsSpinner = (Spinner) findViewById(R.id.spnMonths);
+
+            String msg = "You have enrolled for " + checkedRadiobtn.getText() + " lessons in "
+                    + monthsSpinner.getSelectedItem();
+
+            //Displaying what they have enrolled in
+            TextView tvEnrolled = (TextView) findViewById(R.id.tvOutput);
+            tvEnrolled.setText(msg);
+        }
     }
 }
+
