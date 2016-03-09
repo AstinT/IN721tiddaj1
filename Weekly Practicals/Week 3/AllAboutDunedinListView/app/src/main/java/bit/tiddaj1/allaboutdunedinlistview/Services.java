189660d4881dcb0cb8aea1750baea312990713c7
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-09 15:19:12 +1300

    Initial commit
    Created and setup all layouts and activities

---
+++ Weekly Practicals/Week 3/AllAboutDunedinListView/app/src/main/java/bit/tiddaj1/allaboutdunedinlistview/Services.java
@@ -0,0 +1,13 @@
+package bit.tiddaj1.allaboutdunedinlistview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Services extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subscreen);
+    }
+}
