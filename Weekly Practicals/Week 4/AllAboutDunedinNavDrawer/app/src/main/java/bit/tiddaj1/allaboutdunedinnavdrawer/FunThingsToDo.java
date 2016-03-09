a0f1e8a56779e837147e9fc5e29bbe8c97b41494
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-10 12:49:31 +1300

    Added subScreen java files
    From last project

---
+++ Weekly Practicals/Week 4/AllAboutDunedinNavDrawer/app/src/main/java/bit/tiddaj1/allaboutdunedinnavdrawer/FunThingsToDo.java
@@ -0,0 +1,23 @@
+package bit.tiddaj1.allaboutdunedinnavdrawer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class FunThingsToDo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subscreen);
+
+        //Declaring a TextView and ImageView. Getting there ids
+        TextView subScreenTitle = (TextView) findViewById(R.id.tvSubScreenTitle);
+        ImageView subScreenImage = (ImageView) findViewById(R.id.ivSubScreenImage);
+
+        //Setting text and image for the TextView and ImageView
+        subScreenTitle.setText(R.string.subScreenTitleFunThingsToDo);
+        subScreenImage.setImageResource(R.drawable.monarch);
+    }
+}
