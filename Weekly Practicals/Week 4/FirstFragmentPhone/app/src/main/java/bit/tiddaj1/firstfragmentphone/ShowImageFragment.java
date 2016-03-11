ae721835ddf145c94a19a47253e34666c93e7cac
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-11 15:00:07 +1300

    Added ShowImageFragment class

---
+++ Weekly Practicals/Week 4/FirstFragmentPhone/app/src/main/java/bit/tiddaj1/firstfragmentphone/ShowImageFragment.java
@@ -0,0 +1,20 @@
+package bit.tiddaj1.firstfragmentphone;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ShowImageFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        View fragmentView = inflater.inflate(R.layout.show_image_fragment, container, false);
+
+        return fragmentView;
+    }
+}
