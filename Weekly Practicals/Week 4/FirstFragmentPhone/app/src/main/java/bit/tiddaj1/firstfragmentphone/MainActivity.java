42bad48950de1fb90db9f0b539faa420c2a06b4e
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-11 22:16:26 +1300

    Added buttonListener and buildFragment method

--- Weekly Practicals/Week 4/FirstFragmentPhone/app/src/main/java/bit/tiddaj1/firstfragmentphone/MainActivity.java
+++ Weekly Practicals/Week 4/FirstFragmentPhone/app/src/main/java/bit/tiddaj1/firstfragmentphone/MainActivity.java
@@ -1,5 +1,8 @@
 package bit.tiddaj1.firstfragmentphone;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,5 +16,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button btnImageFragment = (Button) findViewById(R.id.btnShowImage);
+        Button btnListFragment = (Button) findViewById(R.id.btnShowList);
+
+        btnImageFragment.setOnClickListener(new buttonListener());
+        btnListFragment.setOnClickListener(new buttonListener());
+    }
+
+    //Button listener class
+    public class buttonListener implements Button.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+            if(v.getId() == R.id.btnShowImage)
+            {
+                Fragment dynamicFragment = new ShowImageFragment();
+                buildFragment(dynamicFragment);
+            }
+            else
+            {
+                Fragment dynamicFragment = new ShowListFragment();
+                buildFragment(dynamicFragment);
+            }
+        }
+    }
+
+    public void buildFragment(Fragment dynamicFragment) {
+
+        FragmentManager fm = getFragmentManager();
+
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fragment_container, dynamicFragment);
+        ft.commit();
     }
 }
