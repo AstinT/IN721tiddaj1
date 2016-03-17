52f182967ffe0464fc52c2d23d08dced73c74b33
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-17 20:08:31 +1300

    Added MainActivity code

--- Weekly Practicals/Week 5/RequestingDataTextColour/app/src/main/java/bit/tiddaj1/requestingdatatextcolour/MainActivity.java
+++ Weekly Practicals/Week 5/RequestingDataTextColour/app/src/main/java/bit/tiddaj1/requestingdatatextcolour/MainActivity.java
@@ -1,13 +1,54 @@
 package bit.tiddaj1.requestingdatatextcolour;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Global textview
+    TextView tvContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //getting reference to a textview
+        tvContent = (TextView) findViewById(R.id.tvContent);
+
+        //getting reference to button
+        Button btnStartForResult = (Button) findViewById(R.id.btnChangeColour);
+        //setting onClickListener
+        btnStartForResult.setOnClickListener(new StartForResultHandler());
+    }
+
+    //onClickListener
+    public class StartForResultHandler implements View.OnClickListener
+    {
+        @Override
+        public void onClick(View v)
+        {
+            //intent to do work
+            Intent changeActivityIntent = new Intent(MainActivity.this, ChangeTextColour.class);
+
+            //start intent with code
+            startActivityForResult(changeActivityIntent, 0);
+        }
+    }
+
+    //Retrieving the requested data
+    protected void onActivityResult (int requestCode, int resultCode, Intent data)
+    {
+        if ((requestCode == 0) && (resultCode == Activity.RESULT_OK))
+        {
+            //Set colour of text
+            int colour = data.getIntExtra("colour", 0);
+            tvContent.setTextColor(colour);
+        }
     }
 }
