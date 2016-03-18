a02ef61149bd9988727adc15153482706c63cff6
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-19 10:38:57 +1300

    Finishhed main activity

--- Weekly Practicals/Week 5/MusicSchoolDialog/app/src/main/java/bit/tiddaj1/musicschooldialog/MainActivity.java
+++ Weekly Practicals/Week 5/MusicSchoolDialog/app/src/main/java/bit/tiddaj1/musicschooldialog/MainActivity.java
@@ -1,5 +1,7 @@
 package bit.tiddaj1.musicschooldialog;
 
+import android.app.FragmentManager;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,65 +12,101 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    //Global
+    Spinner monthsSpinner;
+    TextView tvEnrolled;
+    AlertBuilderFragment confirm;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Layout for spinner
-        int layoutID = android.R.layout.simple_spinner_item;
-        //Array of strings for spinner
-        String[] months =
-                {
-                        "January",
-                        "February",
-                        "March",
-                        "April",
-                        "May",
-                        "June",
-                        "July",
-                        "August",
-                        "September",
-                        "October",
-                        "November",
-                        "December"
-                };
-        //Creating adapter with layout and strings array
-        ArrayAdapter<String> monthsAdapter = new ArrayAdapter<String>(this, layoutID, months);
-
-        //Declaring spinner
-        Spinner monthsSpinner = (Spinner) findViewById(R.id.spnMonths);
-        //setting adapter to spinner
-        monthsSpinner.setAdapter(monthsAdapter);
-
+        //Getting references
+        tvEnrolled = (TextView) findViewById(R.id.tvOutput);
+        monthsSpinner = (Spinner) findViewById(R.id.spnMonths);
         Button btnEnroll = (Button) findViewById(R.id.btnConfirm);
+
+        setupSpinner();
+
+        //Setting OnClickListener
         btnEnroll.setOnClickListener(new buttonListener());
     }
 
     //Button listener class
-    public class buttonListener implements Button.OnClickListener {
-
+    public class buttonListener implements Button.OnClickListener
+    {
         @Override
         //onClick method.
-        //Executes when button is clicked
-        public void onClick(View v) {
+        //Makes DialogFragment
+        public void onClick(View v)
+        {
+            confirm = new AlertBuilderFragment();
+            FragmentManager fm = getFragmentManager();
+            confirm.show(fm, "confirm");
+        }
+    }
+
+    //Setting up spinner
+    public void setupSpinner()
+    {
+        //Layout for spinner
+        int layoutID = android.R.layout.simple_spinner_item;
+
+        //Getting string array from array.xml
+        Resources resourceMachine = getResources();
+        String[] monthsArray = resourceMachine.getStringArray(R.array.months_array);
+
+        //Creating adapter with layout and string array
+        ArrayAdapter<String> monthsAdapter = new ArrayAdapter<String>(this, layoutID, monthsArray);
+
+        //setting adapter to spinner
+        monthsSpinner.setAdapter(monthsAdapter);
+    }
+
+    //Builds confirmation message depending on what the user clicks in the dialog fragment
+    public void buildConfirmationMessage(boolean option)
+    {
+        if(option)
+        {
             //getting radio group id
             RadioGroup rgInstruments = (RadioGroup) findViewById(R.id.rgInstruments);
+
             //gets checked radiobutton that belongs to rgInstruments
             int checkedId = rgInstruments.getCheckedRadioButtonId();
+
             //Declaring a radioButton with the checkedId
             RadioButton checkedRadiobtn = (RadioButton) findViewById(checkedId);
 
-            Spinner monthsSpinner = (Spinner) findViewById(R.id.spnMonths);
-
+            //Build message
             String msg = "You have enrolled for " + checkedRadiobtn.getText() + " lessons in "
-                    + monthsSpinner.getSelectedItem();
+                    + monthsSpinner.getSelectedItem() + ".";
 
             //Displaying what they have enrolled in
-            TextView tvEnrolled = (TextView) findViewById(R.id.tvOutput);
             tvEnrolled.setText(msg);
+        }
+        else
+            //Click no in dialog fragment
+            tvEnrolled.setText("Ohwell.");
+    }
+
+    //passes data from AlertBuilderFragment to MainActivity
+    public void getDialogData(boolean option)
+    {
+        //Closes dialog fragment
+        confirm.dismiss();
+
+        //Builds a confirmation message
+        if (option)
+        {
+            buildConfirmationMessage(option);
+        }
+        else
+        {
+            buildConfirmationMessage(option);
         }
     }
 }
