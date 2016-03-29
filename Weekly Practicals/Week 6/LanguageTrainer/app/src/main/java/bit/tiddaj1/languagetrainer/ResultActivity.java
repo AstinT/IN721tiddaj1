910943fe6b57c5abaed41f57222295b7f25c0c99
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-29 18:32:49 +1300

    Finished ResultActivity

--- Weekly Practicals/Week 6/LanguageTrainer/app/src/main/java/bit/tiddaj1/languagetrainer/ResultActivity.java
+++ Weekly Practicals/Week 6/LanguageTrainer/app/src/main/java/bit/tiddaj1/languagetrainer/ResultActivity.java
@@ -1,7 +1,11 @@
 package bit.tiddaj1.languagetrainer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -9,5 +13,30 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        displayScore();
+
+        Button btnStartOver = (Button) findViewById(R.id.btnStartOver);
+        btnStartOver.setOnClickListener(new buttonListener());
+    }
+
+    public void displayScore()
+    {
+        TextView tvResult = (TextView) findViewById(R.id.tvResult);
+        Bundle extras = getIntent().getExtras();
+        int score = extras.getInt("score");
+        tvResult.setText(score + " / 11");
+    }
+
+    //Button listener class
+    public class buttonListener implements Button.OnClickListener
+    {
+        @Override
+        //onClick method
+        public void onClick(View v)
+        {
+            Intent goToWelcome = new Intent(ResultActivity.this, WelcomeActivity.class);
+            startActivity(goToWelcome);
+        }
     }
 }
