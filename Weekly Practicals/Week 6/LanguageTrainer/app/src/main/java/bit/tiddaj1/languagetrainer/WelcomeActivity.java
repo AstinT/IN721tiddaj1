afeb275ccd5d7e372f5d85c1272e1d53db3ebc72
Astin Tiddy <Astin8@hotmail.co.nz>
2016-03-29 18:31:32 +1300

    Finished Welcome Activity

--- Weekly Practicals/Week 6/LanguageTrainer/app/src/main/java/bit/tiddaj1/languagetrainer/WelcomeActivity.java
+++ Weekly Practicals/Week 6/LanguageTrainer/app/src/main/java/bit/tiddaj1/languagetrainer/WelcomeActivity.java
@@ -1,7 +1,10 @@
 package bit.tiddaj1.languagetrainer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button btnStart = (Button) findViewById(R.id.btnStart);
+        btnStart.setOnClickListener(new buttonListener());
+    }
+
+    //Button listener class
+    public class buttonListener implements Button.OnClickListener
+    {
+        @Override
+        //onClick method
+        public void onClick(View v)
+        {
+            Intent goToQuestions = new Intent(WelcomeActivity.this, QuestionActivity.class);
+            startActivity(goToQuestions);
+        }
     }
 }
