7b20d33b5af0c661bcbdda9e58ccc0cac414d165
Astin Tiddy <Astin8@hotmail.co.nz>
2016-04-15 00:00:51 +1200

    Added doInBackground code

--- Weekly Practicals/Week 9/LastFmWebService/app/src/main/java/bit/tiddaj1/lastfmwebservice/MainActivity.java
+++ Weekly Practicals/Week 9/LastFmWebService/app/src/main/java/bit/tiddaj1/lastfmwebservice/MainActivity.java
@@ -4,6 +4,12 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -17,12 +23,42 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected String doInBackground(Void... params)
         {
-            
+
+            String JSONString = null;
+            try
+            {
+                String urlString = "";
+
+                URL URLObject = new URL(urlString);
+
+                HttpURLConnection connection = (HttpURLConnection) URLObject.openConnection();
+
+                connection.connect();
+
+                int responseCode = connection.getResponseCode();
+
+                InputStream inputStream = connection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                String responseString;
+                StringBuilder stringBuilder = new StringBuilder();
+                while((responseString = bufferedReader.readLine()) != null)
+                {
+                    stringBuilder = stringBuilder.append(responseString);
+                }
+
+                JSONString = stringBuilder.toString();
+            }
+            catch(Exception e)
+            {
+                e.printStackTrace();
+            }
+            return JSONString;
         }
 
         protected void onPostExecute(String fetchedString)
         {
-
         }
     }
 }
