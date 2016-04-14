73d734444fe79427b019be13d2c78cbdda1bda01
Astin Tiddy <Astin8@hotmail.co.nz>
2016-04-14 13:24:06 +1200

    Added inner class

--- Weekly Practicals/Week 9/LastFmWebService/app/src/main/java/bit/tiddaj1/lastfmwebservice/MainActivity.java
+++ Weekly Practicals/Week 9/LastFmWebService/app/src/main/java/bit/tiddaj1/lastfmwebservice/MainActivity.java
@@ -1,5 +1,6 @@
 package bit.tiddaj1.lastfmwebservice;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public class AsyncAPI extends AsyncTask<Void, Void, String>
+    {
+        @Override
+        protected String doInBackground(Void... params)
+        {
+            
+        }
+
+        protected void onPostExecute(String fetchedString)
+        {
+
+        }
     }
 }
