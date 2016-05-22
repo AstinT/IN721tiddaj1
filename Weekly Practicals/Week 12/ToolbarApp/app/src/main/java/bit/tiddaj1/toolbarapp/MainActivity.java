a799d5ac7459ab67e784e914c3fa12cb634a032c
Astin Tiddy <Astin8@hotmail.co.nz>
2016-05-22 22:46:43 +1200

    Finished practical

--- Weekly Practicals/Week 12/ToolbarApp/app/src/main/java/bit/tiddaj1/toolbarapp/MainActivity.java
+++ Weekly Practicals/Week 12/ToolbarApp/app/src/main/java/bit/tiddaj1/toolbarapp/MainActivity.java
@@ -5,19 +5,27 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView ivPic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Reference to imageview
+        ivPic = (ImageView) findViewById(R.id.ivPic);
     }
 
     @Override
+    //Create tool bars
     public boolean onCreateOptionsMenu(Menu menu)
     {
         MenuInflater menuInflater = getMenuInflater();
+        //inflates from the xml I created
         menuInflater.inflate(R.menu.menu_list, menu);
 
         return super.onCreateOptionsMenu(menu);
@@ -26,11 +34,30 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
+        //Gets selected item title
         String itemTitle = (String) item.getTitle();
 
+        //Checks on the selected item title
         switch (itemTitle)
         {
-            case
+            case "Up":
+                //Moves image up
+                ivPic.setY(ivPic.getY() - 10);
+                break;
+            case "Down":
+                //Moves image down
+                ivPic.setY(ivPic.getY() + 10);
+                break;
+            case "Left":
+                //Moves image left
+                ivPic.setX(ivPic.getX() - 10);
+                break;
+            case "Right":
+                //Moves image right
+                ivPic.setX(ivPic.getX() + 10);
+                break;
         }
+
+        return true;
     }
 }
