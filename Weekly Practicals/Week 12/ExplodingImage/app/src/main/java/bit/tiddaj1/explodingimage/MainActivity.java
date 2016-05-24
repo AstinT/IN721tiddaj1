14ac8e9f9da6134c152279b5035b8018a59bf86e
Astin Tiddy <Astin8@hotmail.co.nz>
2016-05-24 17:32:25 +1200

    Updated ExplodigImage practical

--- Weekly Practicals/Week 12/ExplodingImage/app/src/main/java/bit/tiddaj1/explodingimage/MainActivity.java
+++ Weekly Practicals/Week 12/ExplodingImage/app/src/main/java/bit/tiddaj1/explodingimage/MainActivity.java
@@ -3,6 +3,7 @@ package bit.tiddaj1.explodingimage;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.easyandroidanimations.library.Animation;
@@ -12,7 +13,7 @@ import com.easyandroidanimations.library.ExplodeAnimation;
 public class MainActivity extends AppCompatActivity {
 
     //Global
-    ImageView zonda;
+    private ExplodeAnimation explodeAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,21 +21,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Reference to zonda image
-        zonda = (ImageView) findViewById(R.id.ivZonda);
-        //Set image onClickListener
-        zonda.setOnClickListener(new imageClickHandler());
+        ImageView zonda = (ImageView) findViewById(R.id.ivZonda);
+
+        //New exploding animation, passed image
+        explodeAnimation = new ExplodeAnimation(zonda);
+        //Set listener. Code is ran after animation
+        explodeAnimation.setListener(new animationHandler());
+
+        //Reference to button
+        Button btnExplode = (Button) findViewById(R.id.btnExplode);
+        //Set onClickListener
+        btnExplode.setOnClickListener(new buttonClickHandler());
     }
 
-    public class imageClickHandler implements View.OnClickListener
+    public class buttonClickHandler implements View.OnClickListener
     {
         @Override
         public void onClick(View v)
         {
-            //New exploding animation, passed View v which is my image
-            ExplodeAnimation explodeAnimation = new ExplodeAnimation(v);
-            //Set listener. Code is ran after animation
-            explodeAnimation.setListener(new animationHandler());
-            //Animate the view
+            //Animate
             explodeAnimation.animate();
         }
     }
